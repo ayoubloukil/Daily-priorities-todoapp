17f75316429f8d0e81bf0623618fe072a899b5cc
Ayoub Loukil <Ayoub1953@protonmail.com>
2024-08-04 14:40:32 +0100

    Base app changes done

--- main.js
+++ main.js
@@ -1,42 +1,58 @@
 const submitBtn = document.getElementById("btn-submit");
 const submitInput = document.getElementById("txt-priority");
 const olElement = document.getElementById("ordered-list");
+const olElementConfirm = document.getElementById("ordered-list-confirm");
+const divElementConfirm = document.getElementById("container-confirm-message");
+const errorContainer = document.getElementById("input-err");
 let taskArr = [];
 
 
+
 function addTask(){
-    if (taskArr.length === 0 || taskArr.length < 3){
-        taskArr.push(submitInput.value);
-        let listItem = document.createElement('li');
-        listItem.innerText = submitInput.value;
-        olElement.appendChild(listItem);
-    } else {
-        alert("You have reached the maximum amount of tasks you can submit!")
+    errorContainer.innerHTML = "";
+    if(submitInput.value.trim() !== ""){
+        if (taskArr.length === 0 || taskArr.length < 3){
+            taskArr.push(submitInput.value);
+            let listItem = document.createElement('li');
+            listItem.innerText = submitInput.value;
+            olElement.appendChild(listItem);
+            listItem.style.listStyle = "decimal inside";
+
+        } else {
+            alert("You have reached the maximum amount of tasks you can submit!")
+        }
+        console.log(taskArr)
+        submitInput.value = "";
+    }   else {
+        let errMessage = document.createElement('p');
+        errMessage.innerText = "Submit a task to continue.";
+        errorContainer.appendChild(errMessage);
     }
-    console.log(taskArr)
-    submitInput.value = "";
+
 }
 
 
 function confirmTasks() {
     localStorage.setItem("tasks", JSON.stringify(taskArr));
-    document.getElementById("container-submit").style.display = "none";
     showTasks();
 }
 
 function showTasks(){
-    const getTasks = localStorage.getItem(JSON.parse("tasks"));
-
+    const getTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (getTasks.length != 0) {
+        for (let i = 0; i < getTasks.length; i++) {
+            let listItem = document.createElement('li');
+            listItem.innerText = getTasks[i];
+            olElementConfirm.appendChild(listItem);
+            listItem.style.listStyle = "decimal inside";
+        }
+        document.getElementById("container").style.display = "none";
+        let dailyTaskMessage = document.createElement('h1');
+        dailyTaskMessage.innerText = "Here are your daily tasks";
+        divElementConfirm.appendChild(dailyTaskMessage);
+    }
 }
 
-
-
-
-
- 
-
-
-// function showTask () {
-//     localStorage.getItem('priorities')
-// }
-
+window.onload = function() {
+    showTasks();
+  };
